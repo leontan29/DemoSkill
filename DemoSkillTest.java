6571ca75860ec0fa63ac8d0360045b7bc643b132
leontan29 <97635396+leontan29@users.noreply.github.com>
2022-02-04 12:21:33 -0800

    Update DemoSkillTest.java

--- DemoSkillTest.java
+++ DemoSkillTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class DemoSkillTest {
     @Test
     public void multiplication() {
-        assertEquals(6, DemoSkill.multiply(4, 2)); //initially fails
+        assertEquals(6, DemoSkill.multiply(3, 2)); //initially fails
     }
 }
